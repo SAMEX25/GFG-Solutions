8ca0fe2b0642c42c519daf6d3fc22cb100dcfa9a
Samyak Patil <itssam0025@gmail.com>
2026-07-09 21:40:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] arr) {
+        // code here
+        if(arr == null || arr.length<=1)return;
+        
+        int last = arr[arr.length-1];
+        
+        for(int i=arr.length-1; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = last;
+    }
+}
