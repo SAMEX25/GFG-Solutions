8f4c379f8b2ab9db5e642c0f7d9c2ddd73cec935
Samyak Patil <itssam0025@gmail.com>
2026-07-23 22:14:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insertion in a Doubly Linked List/insertion-in-a-doubly-linked-list.java
@@ -0,0 +1,40 @@
+/* Structure of Doubly Linked List Node
+class Node
+{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+*/
+
+class Solution {
+    Node insertAtPos(Node head, int p, int x) {
+        // code here
+        Node newNode = new Node(x);
+        
+        if(head==null){
+            return newNode;
+        }
+        Node curr = head;
+        for(int i=0; i<p && curr != null; i++){
+            curr = curr.next;
+        }
+        if(curr == null){
+            return head;
+        }
+        newNode.next = curr.next;
+        newNode.prev  = curr;
+        
+        if(curr.next != null){
+            curr.next.prev = newNode;
+        }
+        curr.next = newNode;
+        
+        return head;
+    }
+}
