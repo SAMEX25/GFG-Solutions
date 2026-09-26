87233e4c9285d033a465f6042c666c7eec8f8a07
Samyak Patil <itssam0025@gmail.com>
2026-09-26 21:46:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with 0 Sum/pairs-with-0-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public static ArrayList<ArrayList<Integer>> getPairs(int[] arr) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == 0) {
+                ArrayList<Integer> pair = new ArrayList<>();
+                pair.add(arr[left]);
+                pair.add(arr[right]);
+                result.add(pair);
+
+                // Skip duplicates
+                int leftValue = arr[left];
+                int rightValue = arr[right];
+
+                while (left < right && arr[left] == leftValue) {
+                    left++;
+                }
+
+                while (left < right && arr[right] == rightValue) {
+                    right--;
+                }
+
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
